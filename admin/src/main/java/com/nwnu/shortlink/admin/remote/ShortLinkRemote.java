f06c03817b99ae1155153fc9df5decc4e177da50
kaka <1982950647@qq.com>
2024-03-02 10:58:59 +0800

    新增短链接获取短链接标题和图标功能

--- admin/src/main/java/com/nwnu/shortlink/admin/remote/ShortLinkRemote.java
+++ admin/src/main/java/com/nwnu/shortlink/admin/remote/ShortLinkRemote.java
@@ -11,6 +11,7 @@ import com.nwnu.shortlink.admin.remote.req.ShortLinkPageReqDTO;
 import com.nwnu.shortlink.admin.remote.req.ShortLinkUpdateReqDTO;
 import com.nwnu.shortlink.admin.remote.resp.ShortLinkCreateRespDto;
 import com.nwnu.shortlink.admin.remote.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.List;
@@ -64,5 +65,17 @@ public interface ShortLinkRemote {
      */
     default void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
         HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/update", JSON.toJSONString(requestParam));
+    }
+
+    /**
+     * 根据 URL 获取标题
+     *
+     * @param url 目标网站地址
+     * @return 网站标题
+     */
+    default Result<String> getTitleByUrl(@RequestParam("url") String url) {
+        String resultStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/title?url=" + url);
+        return JSON.parseObject(resultStr, new TypeReference<>() {
+        });
     }
 }
